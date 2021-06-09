3df1c2d76e49da510aecb62dcc0a3a740431756e
timrodenbroeker <post@timrodenbroeker.de>
2021-06-09 12:02:11 +0200

    added data and wrote javascript

---
+++ app.js
@@ -0,0 +1,32 @@
+let theData;
+
+fetch("./data/sites.json")
+    .then((response) => response.json())
+    .then((data) => prepareHTML(data));
+
+function prepareHTML(data) {
+    for (var i = 0; i < data.length; i++) {
+        var dataItem = data[i];
+
+        var item = document.createElement("a");
+        item.setAttribute("href", dataItem.url);
+        item.setAttribute("target", "_blank");
+
+        var img = document.createElement("img");
+        if (dataItem.thumb != false) {
+            img.src = dataItem.thumb;
+        } else {
+            img.src = "./img/nothumbnail.png";
+        }
+
+        item.appendChild(img);
+
+        var title = document.createElement("h3");
+        title.innerHTML = dataItem.title;
+
+        item.appendChild(title);
+
+        var parent = document.getElementById("theGrid");
+        parent.appendChild(item);
+    }
+}
