ab0967aa2d40cf03a42c8055d6519db6480f2dc9
timrodenbroeker <post@timrodenbroeker.de>
2021-06-14 09:31:50 +0200

    added modal interactivity

--- js/modal.js
+++ js/modal.js
@@ -1,86 +1,91 @@
 function toggleModal() {
-  var body = document.getElementsByTagName("body")[0];
-  body.classList.toggle("modalActive");
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.toggle("modalActive");
 }
 
 function collapseModal() {
-  var body = document.getElementsByTagName("body")[0];
-  body.classList.remove("modalActive");
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.remove("modalActive");
 }
 
-document.onkeydown = function (e) {
-  if (e.key === "Escape") {
+var theBody = document.getElementsByTagName("body")[0];
+theBody.onscroll = function () {
     collapseModal();
-  }
+};
+
+document.onkeydown = function (e) {
+    if (e.key === "Escape") {
+        collapseModal();
+    }
 };
 
 var silk = document.getElementById("silk");
 
 silk.addEventListener("click", function (e) {
-  toggleModal();
+    toggleModal();
 });
 
 // Modal Markup
 
 function populizeModal(index) {
-  let dataItem = theData[index];
+    let dataItem = theData[index];
 
-  let browserMockup = document.createElement("div");
-  browserMockup.classList.add("browser-mockup");
+    let browserMockup = document.createElement("div");
+    browserMockup.classList.add("browser-mockup");
 
-  // image
-  let img = document.createElement("img");
+    // image
+    let img = document.createElement("img");
 
-  // link
-  let imgLink = document.createElement("a");
-  imgLink.setAttribute("href", dataItem.url);
-  imgLink.setAttribute("target", "_blank");
+    // link
+    let imgLink = document.createElement("a");
+    imgLink.setAttribute("href", dataItem.url);
+    imgLink.setAttribute("target", "_blank");
 
-  if (dataItem.thumb != false) {
-    img.src = `./img/${dataItem.thumb}`;
-  } else {
-    img.src = "./img/nothumb.jpg";
-  }
-
-  imgLink.appendChild(img);
-
-  browserMockup.appendChild(imgLink);
-
-  let modalHeader = document.getElementById("modalHeader");
-  modalHeader.innerHTML = "";
-  modalHeader.appendChild(browserMockup);
-
-  let modalContent = document.getElementById("modalContent");
-  modalContent.innerHTML = "";
-
-  let title = document.createElement("h2");
-  title.classList.add("lead");
-  title.innerHTML = dataItem.title;
-  modalContent.appendChild(title);
-
-  if (dataItem.artist != false) {
-    if (dataItem.artistWebsite != false) {
-      let by = `<p class="meta">by <a href="${dataItem.artistWebsite}" target="_blank">${dataItem.artist}</a></p>`;
-      modalContent.insertAdjacentHTML("beforeend", by);
+    if (dataItem.thumb != false) {
+        img.src = `./img/${dataItem.thumb}`;
     } else {
-      let by = `<p class="meta">by ${dataItem.artist}</p>`;
-      modalContent.insertAdjacentHTML("beforeend", by);
+        img.src = "./img/nothumb.jpg";
     }
-  }
 
-  if (dataItem.description != false) {
-    let desc = document.createElement("p");
-    desc.append(dataItem.description);
-    modalContent.appendChild(desc);
-  }
+    imgLink.appendChild(img);
 
-  let button = document.createElement("a");
-  button.setAttribute("href", dataItem.url);
-  button.setAttribute("target", "_blank");
-  button.classList.add("btn");
-  button.classList.add("m-top-15");
-  button.innerHTML = "Launch";
-  modalContent.appendChild(button);
+    browserMockup.appendChild(imgLink);
+
+    let modalHeader = document.getElementById("modalHeader");
+    modalHeader.innerHTML = "";
+    modalHeader.appendChild(browserMockup);
+
+    let modalContent = document.getElementById("modalContent");
+    modalContent.innerHTML = "";
+
+    let title = document.createElement("h2");
+    title.classList.add("lead");
+    title.innerHTML = dataItem.title;
+    modalContent.appendChild(title);
+
+    if (dataItem.artist != false) {
+        if (dataItem.artistWebsite != false) {
+            let by = `<p class="meta">by <a href="${dataItem.artistWebsite}" target="_blank">${dataItem.artist}</a></p>`;
+            modalContent.insertAdjacentHTML("beforeend", by);
+        } else {
+            let by = `<p class="meta">by ${dataItem.artist}</p>`;
+            modalContent.insertAdjacentHTML("beforeend", by);
+        }
+    }
+
+    if (dataItem.description != false) {
+        let desc = document.createElement("p");
+        desc.append(dataItem.description);
+        modalContent.appendChild(desc);
+    }
+
+    let button = document.createElement("a");
+    button.setAttribute("href", dataItem.url);
+    button.setAttribute("target", "_blank");
+    button.classList.add("btn");
+    button.classList.add("m-top-15");
+    button.innerHTML = "Launch";
+    modalContent.appendChild(button);
 }
 
 var infoMarkup = `
@@ -97,15 +102,15 @@ var infoMarkup = `
 
 var learnMoreBtn = document.getElementById("learnMore");
 learnMoreBtn.addEventListener("click", function (e) {
-  learnMore();
+    learnMore();
 });
 
 function learnMore() {
-  let modalHeader = document.getElementById("modalHeader");
-  modalHeader.innerHTML = "";
+    let modalHeader = document.getElementById("modalHeader");
+    modalHeader.innerHTML = "";
 
-  let modalContent = document.getElementById("modalContent");
-  modalContent.innerHTML = infoMarkup;
+    let modalContent = document.getElementById("modalContent");
+    modalContent.innerHTML = infoMarkup;
 
-  toggleModal();
+    toggleModal();
 }
